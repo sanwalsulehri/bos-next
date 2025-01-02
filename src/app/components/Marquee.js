cf280d031cc74eedb9c6953f388bccdd26c95f02
sanwalsulehri <sanwalsulehri077@gmail.com>
2025-01-02 05:48:52 +0500

    done first page

--- src/app/components/Marquee.js
+++ src/app/components/Marquee.js
@@ -1,40 +1,41 @@
 import Image from "next/image";
-import React from "react";
-import logo1 from '../assets/logo1.png'
-import logo2 from '../assets/logo2.png'
-import logo3 from '../assets/logo3.png'
-import logo4 from '../assets/logo4.png'
-import logo5 from '../assets/logo5.png'
+import logo1 from '../assets/logo1.png';
+import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
+import logo4 from '../assets/logo4.png';
+import logo5 from '../assets/logo5.png';
 
-const Marquee = () => {
-
-
-
+const MarqueeSlider = () => {
   return (
-    <div className="py-10">
-      <div className="py-[2px] flex items-center px-[37px] relative">
-        <div
-          style={{
-            background: "linear-gradient(to right, #F3F6F700, #F3F6F7)",
-          }}
-          className="h-full absolute top-0 left-0 w-[412px]"
-        ></div>
+    <div className="pt-10 pb-20 2xl:max-w-[1202px] 2xl:mx-auto relative">
+      <div className="w-full overflow-hidden">
+      <div
+  style={{
+    background: "linear-gradient(to left, #F3F6F700, #F3F6F7)",  
+
+  }}
+  className="absolute top-0 left-0 h-full z-[9999] w-[70px] sm:w-[150px] md:w-[412px]"
+>
+</div>
 
 <div
-          style={{
-            background: "linear-gradient(to right, #F3F6F700, #F3F6F7)",
-          }}
-          className="h-full absolute top-0 right-0 w-[412px]"
-        ></div>
+  style={{
+    background: "linear-gradient(to right, #F3F6F700, #F3F6F7)",  
+  }}
+  className="absolute top-0 right-0 h-40 z-[9999] w-[70px] sm:w-[150px] md:w-[412px]"
+>
+</div>
 
-
-
-        <marquee className="flex items-center gap-10" behaviour="scroll" direction="right">
-            <Image src={""} />
+        <marquee behavior="scroll" direction="left" scrollamount="8" className="!flex w-full m gap-10 justify-between">
+          {[logo1, logo2, logo3, logo4, logo1, logo5,logo1, logo2, logo3, logo4, logo1, logo5,logo1, logo2, logo3, logo4, logo1, logo5].map((logo, idx) => (
+            <div key={idx} className="flex-shrink-0 inline-block w-[40%] lg:w-[15%]">
+              <Image src={logo} className="sm:w-[103px] w-[70px] inline-block h-[29px] sm:h-[43px] object-cover" alt={`logo${idx + 1}`} />
+            </div>
+          ))}
         </marquee>
       </div>
     </div>
   );
 };
 
-export default Marquee;
+export default MarqueeSlider;
