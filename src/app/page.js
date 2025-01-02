805221b224244c85476fb1763d793017f1a4b31a
azharmushtaq0099 <azharmushtaq344@gmail.com>
2025-01-02 06:24:10 +0500

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroHome from './components/HeroHome'
+import Tools from './components/Tools'
+import Invoice from './components/Invoice'
+import Footer from './components/Footer.js'
 
 const page = () => {
   return (
@@ -8,6 +11,9 @@ const page = () => {
     <div className='bg-[#F3F6F7]  w-full min-h-screen'>
     <Navbar />
     <HeroHome />
+    <Tools />
+    <Invoice />
+    <Footer />
     </div>
     </>
   )
