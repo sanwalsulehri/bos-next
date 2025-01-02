398117115942ff5827553bf18bdc69d04d15d754
azharmushtaq0099 <azharmushtaq344@gmail.com>
2025-01-02 07:34:36 +0500

    done

--- src/app/pricing/page.js
+++ src/app/pricing/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Agency from '../components/Agency'
+import Essential from'../components/Essential'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <Agency />
+    <Essential />
+    </>
   )
 }
 
